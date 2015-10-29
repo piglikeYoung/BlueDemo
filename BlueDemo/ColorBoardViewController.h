f55cb4ad4ba94075fe91c2dd1ffc4f4da85dead2
piglikeyoung <316046510@qq.com>
2015-10-29 17:11:28 +0800

    增加取色盘界面

---
+++ BlueDemo/ColorBoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  ColorBoardViewController.h
+//  BlueDemo
+//
+//  Created by piglikeyoung on 15/10/29.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorBoardViewController : UIViewController
+
+@end
---
+++ BlueDemo/ColorBoardViewController.m
@@ -0,0 +1,35 @@
+//
+//  ColorBoardViewController.m
+//  BlueDemo
+//
+//  Created by piglikeyoung on 15/10/29.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import "ColorBoardViewController.h"
+
+@interface ColorBoardViewController ()
+
+@end
+
+@implementation ColorBoardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+#pragma mark 横屏设置
+- (BOOL) shouldAutorotate{
+    return YES;
+    
+}
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskLandscapeRight;
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
+    return UIInterfaceOrientationLandscapeRight;
+}
+
+@end
