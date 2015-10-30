73d0b50f368bf91eb2b92167c79f3a12ca308a6b
piglikeyoung <316046510@qq.com>
2015-10-31 01:51:26 +0800

    更换色盘取代码，改为取色的模式来判断选中色块

--- BlueDemo/ColorBoard/UIImage+ColorAtPixel.m
+++ BlueDemo/ColorBoard/UIImage+ColorAtPixel.m
@@ -1,50 +1,15 @@
-/*
- Copyright (c) 2009 Ole Begemann
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
-
-/*
- UIImage+ColorAtPixel.m
- 
- Created by Ole Begemann
- October, 2009
- */
-
-#import <CoreGraphics/CoreGraphics.h>
+//
+//  UIImage+ColorAtPixel.m
+//  ColorPickDemo
+//
+//  Created by piglikeyoung on 15/10/26.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
 
 #import "UIImage+ColorAtPixel.h"
 
-
 @implementation UIImage (ColorAtPixel)
 
-/*
- Returns the color of the image pixel at point. Returns nil if point lies outside the image bounds.
- If the point coordinates contain decimal parts, they will be truncated.
- 
- To get at the pixel data, this method must draw the image into a bitmap context.
- For minimal memory usage and optimum performance, only the specific requested
- pixel is drawn.
- If you need to query pixel colors for the same image repeatedly (e.g., in a loop),
- this approach is probably less efficient than drawing the entire image into memory
- once and caching it.
- */
 - (UIColor *)colorAtPixel:(CGPoint)point {
     // Cancel if point is outside image coordinates
     if (!CGRectContainsPoint(CGRectMake(0.0f, 0.0f, self.size.width, self.size.height), point)) {
@@ -64,16 +29,16 @@
     int bytesPerRow = bytesPerPixel * 1;
     NSUInteger bitsPerComponent = 8;
     unsigned char pixelData[4] = { 0, 0, 0, 0 };
-    CGContextRef context = CGBitmapContextCreate(pixelData, 
+    CGContextRef context = CGBitmapContextCreate(pixelData,
                                                  1,
                                                  1,
-                                                 bitsPerComponent, 
-                                                 bytesPerRow, 
+                                                 bitsPerComponent,
+                                                 bytesPerRow,
                                                  colorSpace,
                                                  kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
     CGColorSpaceRelease(colorSpace);
     CGContextSetBlendMode(context, kCGBlendModeCopy);
-
+    
     // Draw the pixel we are interested in onto the bitmap context
     CGContextTranslateCTM(context, -pointX, pointY-(CGFloat)height);
     CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, (CGFloat)width, (CGFloat)height), cgImage);
@@ -86,5 +51,54 @@
     CGFloat alpha = (CGFloat)pixelData[3] / 255.0f;
     return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
 }
+
+- (RGBType)colorAtPixel2:(CGPoint)point {
+    RGBType rgba = {0,0,0};
+    
+    // Cancel if point is outside image coordinates
+    if (!CGRectContainsPoint(CGRectMake(0.0f, 0.0f, self.size.width, self.size.height), point)) {
+        return rgba;
+    }
+    
+    
+    // Create a 1x1 pixel byte array and bitmap context to draw the pixel into.
+    // Reference: http://stackoverflow.com/questions/1042830/retrieving-a-pixel-alpha-value-for-a-uiimage
+    NSInteger pointX = trunc(point.x);
+    NSInteger pointY = trunc(point.y);
+    CGImageRef cgImage = self.CGImage;
+    NSUInteger width = self.size.width;
+    NSUInteger height = self.size.height;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    int bytesPerPixel = 4;
+    int bytesPerRow = bytesPerPixel * 1;
+    NSUInteger bitsPerComponent = 8;
+    unsigned char pixelData[4] = { 0, 0, 0, 0 };
+    CGContextRef context = CGBitmapContextCreate(pixelData,
+                                                 1,
+                                                 1,
+                                                 bitsPerComponent,
+                                                 bytesPerRow,
+                                                 colorSpace,
+                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+    CGContextSetBlendMode(context, kCGBlendModeCopy);
+    
+    // Draw the pixel we are interested in onto the bitmap context
+    CGContextTranslateCTM(context, -pointX, pointY-(CGFloat)height);
+    CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, (CGFloat)width, (CGFloat)height), cgImage);
+    CGContextRelease(context);
+    
+    // Convert color values [0..255] to floats [0.0..1.0]
+    //    CGFloat red   = (CGFloat)pixelData[0] / 255.0f;
+    //    CGFloat green = (CGFloat)pixelData[1] / 255.0f;
+    //    CGFloat blue  = (CGFloat)pixelData[2] / 255.0f;
+    //    CGFloat alpha = (CGFloat)pixelData[3] / 255.0f;
+    rgba.r = (CGFloat)pixelData[0];
+    rgba.g = (CGFloat)pixelData[1];
+    rgba.b = (CGFloat)pixelData[2];
+    return rgba;
+    //    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+}
+
 
 @end
--- BlueDemo/ColorBoardViewController.m
+++ BlueDemo/ColorBoardViewController.m
@@ -9,6 +9,7 @@
 #import "ColorBoardViewController.h"
 #import "YJHColorPickerHSWheel.h"
 #import "Masonry.h"
+#import "UIImage+ColorAtPixel.h"
 
 @interface ColorBoardViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *circleImageView;
@@ -16,7 +17,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *confirmBtn;
 
 // 色块图片数组
-@property (nonatomic, strong) NSMutableArray *colorList;
+@property (nonatomic, strong) NSArray *colorList;
 
 // 选中色块的tag
 @property (nonatomic, assign) NSInteger currentColorTag;
@@ -24,13 +25,63 @@
 // 是否第一次发送数据
 @property (nonatomic, assign, getter=isFirstSend) BOOL firstSend;
 
+@property (nonatomic,strong) UIImageView *wheelKnobView;
+
 @end
 
 @implementation ColorBoardViewController
 
-- (NSMutableArray *)colorList {
+- (NSArray *)colorList {
     if (!_colorList) {
-        _colorList = [NSMutableArray array];
+        _colorList = @[@"0xee1e25",
+                       @"0xf15b40",
+                       @"0xf58466",
+                       @"0xf9aa8f",
+                       @"0xfaa51a",
+                       @"0xfcb54c",
+                       @"0xfec679",
+                       @"0xffd8a1",
+                       @"0xf4eb21",
+                       @"0xf6ee60",
+                       @"0xf8f18c",
+                       @"0xfaf5b2",
+                       @"0x9aca3b",
+                       @"0xaed361",
+                       @"0xc1dc89",
+                       @"0xd4e6ae",
+                       @"0x71c054",
+                       @"0x8dca76",
+                       @"0xaad595",
+                       @"0xc5e2b5",
+                       @"0x70c6a5",
+                       @"0x8dcfb5",
+                       @"0xaadac6",
+                       @"0xc5e5d7",
+                       @"0x40b9eb",
+                       @"0x70c4ee",
+                       @"0x96d0f2",
+                       @"0xb8ddf6",
+                       @"0x426fb6",
+                       @"0x6683c1",
+                       @"0x889bcf",
+                       @"0xaab7dd",
+                       @"0x5b51a3",
+                       @"0x756bb1",
+                       @"0x9087c0",
+                       @"0xafa9d3",
+                       @"0x8750a0",
+                       @"0x996daf",
+                       @"0xac8bc0",
+                       @"0xafa9d3",
+                       @"0xd0499a",
+                       @"0xd771ad",
+                       @"0xdf92bf",
+                       @"0xe7b3d3",
+                       @"0xed187a",
+                       @"0xf05e90",
+                       @"0xf388a7",
+                       @"0xf7aec0",
+                       @"0xffffff"];
     }
     return _colorList;
 }
@@ -41,6 +92,7 @@
     self.firstSend = YES;
     
     [self p_SetUpWheel];
+    
 }
 
 - (void)dealloc {
@@ -74,32 +126,36 @@
     [self.circleImageView addGestureRecognizer:panGestureRecognizer];
     
     // 敲击手势
+    UIImageView *wheelKnob = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"colorPickerKnob.png"]];
+    [self.circleImageView addSubview:wheelKnob];
+    wheelKnob.hidden = YES;
+    self.wheelKnobView = wheelKnob;
+    
     UITapGestureRecognizer *tapGestureRecognizer;
     tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
     [self.circleImageView addGestureRecognizer:tapGestureRecognizer];
     
-    
     self.circleImageView.userInteractionEnabled = YES;
     
-    for (NSInteger i = 0; i < 3; i++) {
-        // 获取色块图片的名称
-        NSString *imageName = [NSString stringWithFormat:@"circle%zd",i+1];
-        
-        // 添加色块
-        YJHColorPickerHSWheel *iv = [[YJHColorPickerHSWheel alloc] initWithImage:[UIImage imageNamed:imageName]];
-        iv.tag = i;
-        iv.userInteractionEnabled = YES;
-        // 添加到View
-        [self.circleImageView addSubview:iv];
-        [self.colorList addObject:iv];
-        
-        [iv mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.width.equalTo(self.circleImageView.mas_width);
-            make.height.equalTo(self.circleImageView.mas_height);
-            make.top.equalTo(self.circleImageView.mas_top);
-            make.left.equalTo(self.circleImageView.mas_left);
-        }];
-    }
+//    for (NSInteger i = 0; i < 3; i++) {
+//        // 获取色块图片的名称
+//        NSString *imageName = [NSString stringWithFormat:@"circle%zd",i+1];
+//        
+//        // 添加色块
+//        YJHColorPickerHSWheel *iv = [[YJHColorPickerHSWheel alloc] initWithImage:[UIImage imageNamed:imageName]];
+//        iv.tag = i;
+//        iv.userInteractionEnabled = YES;
+//        // 添加到View
+//        [self.circleImageView addSubview:iv];
+//        [self.colorList addObject:iv];
+//        
+//        [iv mas_makeConstraints:^(MASConstraintMaker *make) {
+//            make.width.equalTo(self.circleImageView.mas_width);
+//            make.height.equalTo(self.circleImageView.mas_height);
+//            make.top.equalTo(self.circleImageView.mas_top);
+//            make.left.equalTo(self.circleImageView.mas_left);
+//        }];
+//    }
     
 }
 
@@ -109,27 +165,54 @@
             return;
         }
         CGPoint tapPoint = [sender locationOfTouch:0 inView:self.circleImageView];
-        for (YJHColorPickerHSWheel *colorIv in self.colorList) {
-            if ([colorIv pointInside:tapPoint withEvent:nil]) {
+//        CGPoint tapPoint = [sender locationInView:self.circleImageView];
+//        for (YJHColorPickerHSWheel *colorIv in self.colorList) {
+//            if ([colorIv pointInside:tapPoint withEvent:nil]) {
+//                // 第一次发送数据不用判断值是否相同
+//                if (self.isFirstSend) {
+//                    _currentColorTag = colorIv.tag;
+//                    __weak typeof(self) weakSelf = self;
+//                    // 回调block
+//                    self.confirmBlock(weakSelf.currentColorTag);
+//                    self.firstSend = NO;
+//                } else {
+//                    // 防止传输速度过快，当值不同的时候才回调
+//                    if (_currentColorTag != colorIv.tag) {
+//                        _currentColorTag = colorIv.tag;
+//                        __weak typeof(self) weakSelf = self;
+//                        // 回调block
+//                        self.confirmBlock(weakSelf.currentColorTag);
+//                    }
+//                }
+//            }
+//            
+//        }
+        
+        RGBType rgba = [self.circleImageView.image colorAtPixel2:tapPoint];
+        NSInteger hex = RGB_to_HEX(rgba.r, rgba.g, rgba.b);
+        NSString *hexString = [NSString stringWithFormat:@"0x%06lx", (long)hex];
+        [self.colorList enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if ([obj isEqualToString:hexString]) {
+//                NSLog(@"%zd",idx);
                 // 第一次发送数据不用判断值是否相同
                 if (self.isFirstSend) {
-                    _currentColorTag = colorIv.tag;
+                    _currentColorTag = idx;
                     __weak typeof(self) weakSelf = self;
                     // 回调block
                     self.confirmBlock(weakSelf.currentColorTag);
                     self.firstSend = NO;
                 } else {
                     // 防止传输速度过快，当值不同的时候才回调
-                    if (_currentColorTag != colorIv.tag) {
-                        _currentColorTag = colorIv.tag;
+                    if (_currentColorTag != idx) {
+                        _currentColorTag = idx;
                         __weak typeof(self) weakSelf = self;
                         // 回调block
                         self.confirmBlock(weakSelf.currentColorTag);
                     }
                 }
             }
-            
-        }
+        }];
+        
     }
 }
 
@@ -139,28 +222,54 @@
             return;
         }
         CGPoint tapPoint = [sender locationOfTouch:0 inView:self.circleImageView];
-        for (YJHColorPickerHSWheel *colorIv in self.colorList) {
-            if ([colorIv pointInside:tapPoint withEvent:nil]) {
-                
+//        for (YJHColorPickerHSWheel *colorIv in self.colorList) {
+//            if ([colorIv pointInside:tapPoint withEvent:nil]) {
+//                
+//                // 第一次发送数据不用判断值是否相同
+//                if (self.isFirstSend) {
+//                    _currentColorTag = colorIv.tag;
+//                    __weak typeof(self) weakSelf = self;
+//                    // 回调block
+//                    self.confirmBlock(weakSelf.currentColorTag);
+//                    self.firstSend = NO;
+//                } else {
+//                    // 防止传输速度过快，当值不同的时候才回调
+//                    if (_currentColorTag != colorIv.tag) {
+//                        _currentColorTag = colorIv.tag;
+//                        __weak typeof(self) weakSelf = self;
+//                        // 回调block
+//                        self.confirmBlock(weakSelf.currentColorTag);
+//                    }
+//                }
+//                
+//            }
+//        }
+
+        
+        RGBType rgba = [self.circleImageView.image colorAtPixel2:tapPoint];
+        NSInteger hex = RGB_to_HEX(rgba.r, rgba.g, rgba.b);
+        NSString *hexString = [NSString stringWithFormat:@"0x%06lx", (long)hex];
+        [self.colorList enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if ([obj isEqualToString:hexString]) {
+                NSLog(@"%zd",idx);
                 // 第一次发送数据不用判断值是否相同
                 if (self.isFirstSend) {
-                    _currentColorTag = colorIv.tag;
+                    _currentColorTag = idx;
                     __weak typeof(self) weakSelf = self;
                     // 回调block
                     self.confirmBlock(weakSelf.currentColorTag);
                     self.firstSend = NO;
                 } else {
                     // 防止传输速度过快，当值不同的时候才回调
-                    if (_currentColorTag != colorIv.tag) {
-                        _currentColorTag = colorIv.tag;
+                    if (_currentColorTag != idx) {
+                        _currentColorTag = idx;
                         __weak typeof(self) weakSelf = self;
                         // 回调block
                         self.confirmBlock(weakSelf.currentColorTag);
                     }
                 }
-                
             }
-        }
+        }];
     }
 }
 
