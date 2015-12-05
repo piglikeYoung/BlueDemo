818b19b2f2aef737e9ddf6f89acdc1ba090484f7
piglikeyoung <316046510@qq.com>
2015-12-05 18:57:25 +0800

    选择多组界面完成

---
+++ BlueDemo/JHGroupButtonView.h
@@ -0,0 +1,13 @@
+//
+//  JHGroupButtonView.h
+//  BlueDemo
+//
+//  Created by piglikeyoung on 15/12/5.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JHGroupButtonView : UIView
+
+@end
---
+++ BlueDemo/JHGroupButtonView.m
@@ -0,0 +1,21 @@
+//
+//  JHGroupButtonView.m
+//  BlueDemo
+//
+//  Created by piglikeyoung on 15/12/5.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import "JHGroupButtonView.h"
+
+@implementation JHGroupButtonView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
