801af65b79530744f2739d812b3c744968181ace
piglikeyoung <316046510@qq.com>
2016-05-06 13:58:19 +0800

    添加简单单元测试

--- BlueDemoTests/BlueDemoTests.m
+++ BlueDemoTests/BlueDemoTests.m
@@ -24,16 +24,8 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void) testOnePlusOneEqualsTwo {
+    XCTAssertEqual(1 + 1, 2, "one plus one should equal two");
 }
 
 @end
